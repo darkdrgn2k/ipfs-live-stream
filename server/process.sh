30b5a79cfeb27bfc46dac0b60c09cb7f785338c3
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2018-05-31 12:51:02 -0400

    Indent and missing done

--- server/process.sh
+++ server/process.sh
@@ -1,28 +1,23 @@
 #!/bin/sh
-#rm log
-#rm -rf *.ts
 
 what='LIVE'
-while true; do
-x=`ls $what*.ts`
 
-if ! [ -z "$x" ]; then
-  #Wait for file to finish writing
+while true; do
+    x=`ls $what*.ts`
+
+    if ! [ -z "$x" ]; then
+        #Wait for file to finish writing
         inotifywait -e close_write $x
-  #Add it to IPFS and log the hash
+        #Add it to IPFS and log the hash
         l=`cat $what.m3u8  | grep -B1 $x | grep "#" | awk -F : '{print $2}' | tr -d ,`
         t=`date '+%Y-%m-%d %H:%M:%S'`
         ipfs add $x >> log
         sed -i  "s#$x#$x $l $t#" log
 
 
-  #Move the file to an old folder... you could also just delete it at this point
+        #remove the file
         rm $x
-  # Trim log to only the last 50 entries not to make it too long
-#       mv log logold
-#       tail -n 50 logold > log
-
-  # Re-write teh M3U8 file with the new ipfs hashes from the LOG
+        # Re-write teh M3U8 file with the new ipfs hashes from the LOG
         cp $what.m3u8 test.m3u8
         while read p; do
           h=$(echo "$p" | awk '{print $2}') #Hash
@@ -31,16 +26,17 @@ if ! [ -z "$x" ]; then
         done <log
 
         sed -i "s/EXTM3U/EXTM3U\n#EXT-X-PLAYLIST-TYPE:EVENT/" test.m3u8
-  # Upload to the client
+        # Upload to the client
 
-# M3U8 yo ipns code (noit working)
+        # M3U8  ipns code (noit working)
 #       h=$(ipfs add test.m3u8 | awk '{print $2}')
 #       p=$(ipfs name publish $h)
 #       echo $p
 
 #SCP Code
         scp test.m3u8 user@node2.e-mesh.net:/var/www/html/tv/stream.m3u8
-  # Possible Test - Fore the gateway to grab a copy right away
-#       wget https://ipfs.io/ipfs/$h -o /dev/null -T 3 &
-fi
 
+        # Possible Test - Fore the gateway to grab a copy right away
+#       wget https://ipfs.io/ipfs/$h -o /dev/null -T 3 &
+    fi
+done
